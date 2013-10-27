d174c98d427b8dd2680b3069fc2528f0f31cc6b0
ethanlim <mail@ethanlim.net>
2013-10-27 19:10:28 +0800

    Implemented Cache constructor

--- CS4223/Processor/Cache.cpp
+++ CS4223/Processor/Cache.cpp
@@ -1,9 +1,19 @@
 #include "Cache.h"
 
-CS4223::Processor::Cache::Cache(){
+namespace CS4223{
 
-}
+	Processor::Cache::Cache(Bus * const sharedBus,Protocol::Type protocol_type,unsigned int size, unsigned short assoc, unsigned int blk_size):
+		_sharedBus(sharedBus),_size(size),_assoc(assoc),_blk_size(blk_size)
+	{
+		if(protocol_type==Protocol::MESI){
+			this->protocol = new CS4223::Protocols::MESI();
+		}else if(protocol_type==Protocol::DRAGON){
+			this->protocol = new CS4223::Protocols::DRAGON();
+		}
+	}
 
-CS4223::Processor::Cache::~Cache(){
+	Processor::Cache::~Cache(){
+
+	}
 
 }
--- CS4223/Processor/Cache.h
+++ CS4223/Processor/Cache.h
@@ -1,15 +1,40 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#include "../Bus.h"
+#include "../Protocol.h"
+
+#include "../Protocols/MESI.h"
+#include "../Protocols/DRAGON.h"
+
 using namespace std;
 
 namespace CS4223{
 	namespace Processor{
 		class Cache{
 			private:
+				unsigned int _size;
+				unsigned int _blk_size;
+				unsigned short _assoc;
+				Bus * const _sharedBus;
+				Protocol *protocol;
 			public:
-				Cache();
+				Cache(Bus * const sharedBus,Protocol::Type protocol_type, unsigned int size, unsigned short assoc,unsigned int blk_size);
 				~Cache();
+				class CacheException{
+					public:
+						CacheException(string arg_type,const string msg): _type(arg_type),_msg(msg) {}
+						~CacheException() {}
+						string getType(){
+							return _type;
+						}
+						string getMessage(){
+							return _type+":"+_msg;
+						}
+					private:
+						string _msg;
+						string _type;
+				};
 			};
 	}
 }
