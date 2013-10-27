49924846e0b0546afaf810dc52a931e5537be359
ethanlim <mail@ethanlim.net>
2013-10-27 19:10:39 +0800

    Added Bus values

--- CS4223/Bus.h
+++ CS4223/Bus.h
@@ -1,12 +1,16 @@
 #ifndef BUS_H
 #define BUS_H
 
+#include <string>
+
 using namespace std;
 
 namespace CS4223{
 
 	class Bus{
 		private:
+			bool S;
+			string address;
 		public:
 			Bus();
 			~Bus();
