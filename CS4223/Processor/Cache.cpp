21afaabe7dd33848f9fc2520d5abae1996dc77da
ethanlim <mail@ethanlim.net>
2013-11-05 07:10:29 +0800

    Implemented untested cache

--- CS4223/Processor/Cache.cpp
+++ CS4223/Processor/Cache.cpp
@@ -1,33 +1,194 @@
 #include "Cache.h"
 
 namespace CS4223{
-
-	Processor::Cache::Cache(Bus * const sharedBus,unsigned int size,unsigned short assoc,unsigned int blk_size):
+	namespace Processor{
+		
+		Cache::Cache(Bus *sharedBus, unsigned int size,unsigned short assoc,unsigned int blk_size):
 		_sharedBus(sharedBus),
 		_size(size),
 		_assoc(assoc),
 		_blk_size(blk_size)
-	{
-		unsigned int numOfBlks = this->_size/this->_blk_size;
-		unsigned int cacheLines = this->_size/this->_assoc;
+		{
+			//Given in assignment paper
+			this->_memory_addr_len_bits = 32;
+			this->_word_size_bytes = 2;
 
-		vector<vector<Block>> cache(cacheLines);
+			this->_num_of_blks = this->_size/this->_blk_size;
+			this->_cache_sets = this->_size/this->_assoc;
 
-		for(unsigned int cacheLine=0;cacheLine<cacheLines;cacheLine+=1){
-			cache[cacheLine] = vector<Block>(this->_assoc,Block(this->_blk_size));
+			this->_N_offset_len_bits = this->powerOf2(this->_blk_size/this->_word_size_bytes); 
+			this->_M_cache_set_len_bits = this->powerOf2(this->_cache_sets);
+			this->_tag_len_bits = this->_memory_addr_len_bits - (this->_N_offset_len_bits+this->_M_cache_set_len_bits);
+
+			vector<vector<Block>> cache(this->_cache_sets);
+
+			for(unsigned int set=0;set<this->_cache_sets;set+=1){
+
+				//Each cache set have m number of cache blocks
+				cache[set] = vector<Block>(this->_assoc,Block(this->_blk_size,new Protocol::State()));
+			}
+
+			this->_cache = &cache;
 		}
 
-		this->_cache = &cache;
+		Cache::~Cache(){
+
+		}
+
+		/**
+		*	Returns a 32 bit binary string
+		*/
+		string Cache::conHexToBin(string hex){
+			string sReturn = "";
+			for (int i = 0; i < hex.length (); ++i)
+			{
+				switch (hex [i])
+				{
+					case '0': sReturn.append ("0000"); break;
+					case '1': sReturn.append ("0001"); break;
+					case '2': sReturn.append ("0010"); break;
+					case '3': sReturn.append ("0011"); break;
+					case '4': sReturn.append ("0100"); break;
+					case '5': sReturn.append ("0101"); break;
+					case '6': sReturn.append ("0110"); break;
+					case '7': sReturn.append ("0111"); break;
+					case '8': sReturn.append ("1000"); break;
+					case '9': sReturn.append ("1001"); break;
+					case 'a': sReturn.append ("1010"); break;
+					case 'b': sReturn.append ("1011"); break;
+					case 'c': sReturn.append ("1100"); break;
+					case 'd': sReturn.append ("1101"); break;
+					case 'e': sReturn.append ("1110"); break;
+					case 'f': sReturn.append ("1111"); break;
+				}
+			}
+			return sReturn;
+		}
+
+		unsigned int Cache::conBinToUInt(string bin){
+				unsigned int uint = 0;
+
+				for(int i = 0; i < bin.size(); i++)
+				{
+					if(bin[bin.size() - 1 - i] == '1')
+						uint +=  pow(2 , i) * 1;
+				}
+
+				return uint; 
+		}
+
+		string Cache::extractTag(string address){
+
+			string binary = this->conHexToBin(address);
+
+			string tag = binary.substr(0,this->_tag_len_bits);
+
+			return tag;
+		}
+
+		unsigned int Cache::extractCacheSetIdx(string address){
+			string binary = this->conHexToBin(address);
+
+			string cache_set = binary.substr(this->_tag_len_bits,this->_M_cache_set_len_bits);
+
+			unsigned int cacheSetIdx=this->conBinToUInt(cache_set);
+
+			return cacheSetIdx;
+		}
+
+		unsigned int Cache::extractOffset(string address){
+			string binary = this->conHexToBin(address);
+
+			string offset = binary.substr(this->_M_cache_set_len_bits,this->_memory_addr_len_bits);
+
+			unsigned int offsetIdx = this->conBinToUInt(offset);
+
+			return offsetIdx;
+		}
+
+		double Cache::get_miss_ratio(){
+			unsigned int misses = this->_mem_access - this->_hit;
+
+			return (double) misses/this->_mem_access;
+		}
+		
+		unsigned short Cache::powerOf2(unsigned int num){
+
+			double base = 2
+				,ans = 0
+				,exp = 1;
+
+
+			while(1){
+				
+				ans = pow(base,exp);
+
+				if(ans==num){
+					return exp;
+				}
+
+				exp+=1;
+			}
+		}
+
+		bool Cache::read(string address){
+
+			//Add to memory access
+			this->_mem_access+=1;
+
+			//Extract block identifier
+			string tag = this->extractTag(address);
+			unsigned int cacheIdx = this->extractCacheSetIdx(address);
+			unsigned int offset = this->extractOffset(address);
+			vector<Block> cacheSet = this->_cache->at(cacheIdx);
+			string extracted_tag="";
+			bool miss=true;
+
+			for(unsigned int sets=0;sets<cacheSet.size();sets++){
+
+				//A Hit
+				if(tag==cacheSet[sets].get_tag()&&cacheSet[sets].get_valid()==true){
+
+					miss=false;
+
+				}
+
+			}
+
+			if(miss){
+
+			}else{
+				this->_hit+=1;
+			}
+
+			return !miss;
+		}
+
+		void Cache::fetch(string address){
+			//Extract block identifier
+			string tag = this->extractTag(address);
+			unsigned int cacheIdx = this->extractCacheSetIdx(address);
+			unsigned int offset = this->extractOffset(address);
+			vector<Block> *cacheSet = &this->_cache->at(cacheIdx);
+			
+			//Random Replacement 
+			unsigned int random_blk_idx = rand()%cacheSet->size()+0;
+			Block *selectedBlk = &cacheSet->at(random_blk_idx);
+			
+			if(selectedBlk->get_dirty()){
+				//flush to memory
+				Transaction new_transaction(address);
+				this->_sharedBus->add_transaction(new_transaction);
+
+			}
+
+			selectedBlk->set_tag(tag);
+			selectedBlk->set_valid(true);
+			selectedBlk->set_dirty(false);
+		}
+
+		void Cache::write(string address){
+			
+		}
 	}
-
-	Processor::Cache::~Cache(){
-
-	}
-
-	double Processor::Cache::get_miss_ratio(){
-		unsigned int misses = this->_mem_access - this->_hit;
-
-		return (double) misses/this->_mem_access;
-	}
-
 }
--- CS4223/Processor/Cache.h
+++ CS4223/Processor/Cache.h
@@ -2,10 +2,12 @@
 #define CACHE_H
 
 #include <vector>
+#include <cmath>
+#include <stdlib.h>
 
 #include "../Bus.h"
 #include "../Protocol.h"
-
+#include "Transaction.h"
 
 using namespace std;
 
@@ -13,28 +15,74 @@ namespace CS4223{
 	namespace Processor{
 		class Cache{
 			private:
+				//Cache Info
+				Bus *_sharedBus;
 				unsigned int _size;
 				unsigned int _blk_size;
 				unsigned short _assoc;
-				Bus * const _sharedBus;
+				unsigned int _cache_sets;
+				unsigned int _num_of_blks;
+
+				unsigned short _memory_addr_len_bits;
+				unsigned short _N_offset_len_bits;
+				unsigned short _M_cache_set_len_bits;
+				unsigned short _tag_len_bits;
+				unsigned short _word_size_bytes;
+
+				//Analytics
 				unsigned int _hit;
 				unsigned int _mem_access;
-				
+
 				class Block{
 					private:
 						unsigned int _size;
+						bool Valid;
+						bool Dirty;
+						string Tag;
+						Protocol::State *_state;
 					public:
-						Block(unsigned int size): _size(size){
+						Block(unsigned int size, Protocol::State *state): _size(size){
+							this->_state = state;
+							this->Valid = false;
+							this->Dirty = false;
+							this->Tag = "";
+						}
+						void set_valid(bool valid){
+							this->Valid=valid;
+						}
+						bool get_valid(){
+							return this->Valid;
+						}
+						void set_dirty(bool dirty){
+							this->Dirty = dirty;
+						}
+						bool get_dirty(){
+							return this->Dirty;
+						}
+						void set_tag(string tag){
+							this->Tag = tag;
+						}
+						string get_tag(){
+							return this->Tag;
 						}
 				};
 
 				vector<vector<Block>> *_cache;
+
+				string conHexToBin(string hex);
+				unsigned int conBinToUInt(string bin);
+				unsigned short powerOf2(unsigned int num);
+
+				string extractTag(string hex);
+				unsigned int extractCacheSetIdx(string hex);
+				unsigned int extractOffset(string hex);
+
 			public:
-				Cache(Bus * const sharedBus, unsigned int size, unsigned short assoc,unsigned int blk_size);
+				Cache(Bus *bus, unsigned int size, unsigned short assoc,unsigned int blk_size);
 				~Cache();
-				bool flush(string address);
-				bool write(string address);
 				bool read(string address);
+				void fetch(string address);
+				void write(string address);
 				double get_miss_ratio();
 			};
 	}
