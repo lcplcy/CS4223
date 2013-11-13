21502d0b3e5dc63df7fa49023714bb90ab6e12cc
ethanlim <mail@ethanlim.net>
2013-11-13 16:05:28 +0800

    Remove the shared line variable

--- CS4223/Bus.h
+++ CS4223/Bus.h
@@ -20,12 +20,6 @@ namespace CS4223{
 			unsigned short _bus_up_data_bytes;
 			double _total_address_traffic;
 			double _total_data_traffic;
-			//DRAGON
-			bool shared_line;
-			//If read miss, other cache will use this shared_line to indicate that they have
-			//the data, and pass the data to the requesting cache.
-			//If write hit, and if other cache assert the shared_line to indicate they also have
-			//the data, Change to Shared-Dirty and change all others to Shared-Clean.
 		public:
 			Bus();
 			~Bus();
@@ -44,9 +38,6 @@ namespace CS4223{
 			void unset_shared_line();
 			double get_total_address_traffic();
 			double get_total_data_traffic();
-			//If a cache has a memory address that is being requested via BusRd, assert the shared line and
-			//return the respective cache that has the memory address.
-			void assert_shared_line();
 	};
 }
 #endif
